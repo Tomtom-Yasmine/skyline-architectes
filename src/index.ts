675f7eac756f08d323e858f5f3bf56d7a245e650
Thomas Léveillé <leveille.thomas77@gmail.com>
2023-06-25 13:47:42 +0200

    feat: using ip hash instead of plain ip

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import securityMiddleware from './middleware/security';
 import authRouter from './routers/auth';
 
 dotenv.config();
@@ -8,11 +9,19 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(
+    securityMiddleware.hashIpAddress(),
     express.json(),
     express.urlencoded({ extended: true }),
 );
 
 app.use(authRouter);
+
+app.get('/', (req, res) => {
+    res.json({
+        ip: req.ip,
+        ipHash: req.ipHash,
+    });
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}.`);
