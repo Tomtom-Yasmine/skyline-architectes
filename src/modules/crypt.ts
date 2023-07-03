8c3962ed389511899d2a2559703a1712f4b23819
Thomas Léveillé <leveille.thomas77@gmail.com>
2023-07-03 13:22:19 +0200

    feat: hashing

---
+++ src/modules/crypt.ts
@@ -0,0 +1,47 @@
+import bcrypt from 'bcrypt';
+import crypto from 'node:crypto';
+
+const defaultHashAlgorithm = <string>process.env.HASH_ALGO;
+const passwordHashAlgorithm = process.env.PASSWORD_HASH_ALGO || defaultHashAlgorithm;
+
+type HashAlgorithm = {
+    hash: (value: string) => Promise<string>;
+    verify: (value: string, hash: string) => Promise<boolean>;
+};
+
+const supportedHashAlgorithms: { [key: string]: HashAlgorithm } = {
+    bcrypt: {
+        hash: (value) => bcrypt.hash(value, 10),
+        verify: (value, hash) => bcrypt.compare(value, hash),
+    },
+    ...crypto.getHashes().reduce((acc, hashAlgorithm) => {
+        acc[hashAlgorithm] = {
+            hash: (value) => new Promise((resolve) => {
+                resolve(crypto.createHash(hashAlgorithm).update(value).digest('hex'));
+            }),
+            verify: (value, hash) => new Promise((resolve) => {
+                resolve(crypto.createHash(hashAlgorithm).update(value).digest('hex') === hash);
+            }),
+        };
+        return acc;
+    }, {} as { [key: string]: HashAlgorithm }),
+};
+
+const hash = (value: string, algorithm: string): Promise<string> => {
+    return supportedHashAlgorithms[algorithm].hash(value);
+};
+
+const verify = (value: string, hash: string, algorithm: string): Promise<boolean> => {
+    return supportedHashAlgorithms[algorithm].verify(value, hash);
+};
+
+const forPassword = (): HashAlgorithm => {
+    return supportedHashAlgorithms[passwordHashAlgorithm];
+};
+
+export default {
+    supportedHashAlgorithms,
+    hash,
+    verify,
+    forPassword,
+};
