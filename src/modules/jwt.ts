0810ca902debdd425339967c84654653b1d41b51
Thomas Léveillé <leveille.thomas77@gmail.com>
2023-07-04 11:00:52 +0200

    fix: major jwt improvements

--- src/modules/jwt.ts
+++ src/modules/jwt.ts
@@ -1,34 +1,68 @@
 import {
     User,
 } from '@prisma/client';
+import dayjs from 'dayjs';
 import jwt from 'jsonwebtoken';
 
-const jwtSecret = <string>process.env.SECRET_KEY;
+const getJwtSecret = () => <string>process.env.SECRET_KEY;
 
-type JwtFactory = {
+type JwtFactory<T> = {
     sign: (options?: object) => string;
-    verify: (token: string) => jwt.JwtPayload | string;
+    verify: (token: string) => T;
 };
 
-const forUserSession = (user: User): JwtFactory => {
+enum JwtPurpose {
+    UserSession = 'user-session',
+    EmailVerification = 'email-verification',
+    PasswordReset = 'password-reset',
+}
+
+const forUserSession = (user?: User): JwtFactory<{
+    user: {
+        id: string;
+        role: string;
+    };
+    expirationDate: dayjs.Dayjs;
+}> => {
     return {
         sign: (): string => {
+            if (! user) {
+                throw new Error('ERR:SUBJECT_USER_REQUIRED_TO_CREATE_SESSION_TOKEN');
+            }
             return jwt.sign(
                 {
+                    purpose: 'user-session',
                     role: user.role,
                 },
-                jwtSecret,
+                getJwtSecret(),
                 {
                     subject: user.id,
                     expiresIn: '365d',
                 },
             );
         },
-        verify: (token: string): jwt.JwtPayload | string => {
-            return jwt.verify(
+        verify: (token: string) => {
+            const decoded = jwt.verify(
                 token,
-                jwtSecret,
+                getJwtSecret(),
+                {
+                    subject: user?.id,
+                    complete: true,
+                },
             );
+            console.log(decoded);
+            if (typeof decoded.payload === 'string'
+            || ! decoded.payload.sub
+            || decoded.payload.purpose !== JwtPurpose.UserSession) {
+                throw new Error('ERR:INVALID_SESSION_TOKEN');
+            }
+            return {
+                user: {
+                    id: decoded.payload.sub,
+                    role: decoded.payload.role,
+                },
+                expirationDate: dayjs(decoded.payload.exp),
+            };
         },
     };
 };
@@ -38,14 +72,14 @@ export default {
     sign: (payload: jwt.JwtPayload, options?: jwt.SignOptions): string => {
         return jwt.sign(
             payload,
-            jwtSecret,
+            getJwtSecret(),
             options,
         );
     },
     verify: (token: string): jwt.JwtPayload | string => {
         return jwt.verify(
             token,
-            jwtSecret,
+            getJwtSecret(),
         );
     },
 };
