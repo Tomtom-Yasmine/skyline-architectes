4c2a9faa6e345d0c93e8d26dc1004e9967dedfad
Yasuu19 <yasminegherbibahia@gmail.com>
2023-08-08 22:43:52 +0200

    feat(deploy): add migration

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 git pull --rebase
 docker stop  skyline-architectes
 docker rm  skyline-architectes
+docker rm skyline-migrate
+docker build -t skyline-migrate -f Dockerfile.migrate .
+docker run -it --name  skyline-migrate --network petit  skyline-migrate
 docker build -t  skyline-architectes .
 docker run -dit --name  skyline-architectes --ip 172.18.0.34 --network petit  skyline-architectes
